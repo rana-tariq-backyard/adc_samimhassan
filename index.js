952a0a636e047d338e0ef96b5d2b6133c1ab76f7
Rana Tariq Yaqub <bitsworking@gmail.com>
2024-12-01 03:07:36 +0500

    adding firestore keys

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ app.post('/submit-event', async (req, res) => {
     // Save the data to Firestore
     const eventRef = db.collection('samim_hassan_events').doc();
     await eventRef.set({
-      event_title,
-      event_type,
-      organization_name,
-      contact_email,
-      e_start_date,
-      e_end_date,
-      created_at: new Date(),
+        eventTitle,
+        eventType,
+        organizationName,
+        contactEmail,
+        startsAt,
+        endsAt,
+        created_at: new Date(),
     });
 
     res.status(200).send({ message: 'Event submitted successfully!' });
